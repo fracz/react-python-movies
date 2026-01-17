fc1d2056d9533f3dac1bcde0de766a132d15fa93
Wojciech Frącz <fraczwojciech@gmail.com>
2026-01-17 08:57:42 +0100

    Switch to FAST API

--- api/main.py
+++ api/main.py
@@ -1,44 +1,127 @@
-from typing import List
+from fastapi import FastAPI, Body
+import requests
+from pydantic import BaseModel
+from typing import Any
+import sqlite3
 
-from fastapi import FastAPI, HTTPException
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 
-import schemas
-import models
+class Movie(BaseModel):
+    title: str
+    year: str
+    actors: str
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="../ui/build/static", check_dir=False), name="static")
-
 
 @app.get("/")
-def serve_react_app():
-    return FileResponse("../ui/build/index.html")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/sum")
+def sum(x: int = 0, y: int = 10):
+    return x+y
+
+@app.get("/subtract")
+def sum(x: int, y: int):
+    return x-y
+
+@app.get("/multiply")
+def sum(x: int, y: int):
+    return x*y
+
+@app.get("/geocode")
+async def sum(lat: float, lon: float):
+    # response = requests.get(f"https://geocode.xyz/{lat},{lon}?geoit=json")
+    # response = requests.get(f"https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}",  headers={"User-Agent": "Mozilla/5.0"})
+    response = requests.get(f"https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={lat}&lon={lon}", headers={"User-Agent": "Mozilla/5.0"})
+    # https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521
+    return response.json()
 
 
-@app.get("/movies", response_model=List[schemas.Movie])
-def get_movies():
-    return list(models.Movie.select())
+@app.get('/movies')
+def get_movies():  # put application's code here
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    movies = cursor.execute('SELECT * FROM movies')
+
+    output = []
+    for movie in movies:
+         movie = {'id': movie[0], 'title': movie[1], 'year': movie[2], 'actors': movie[3]}
+         output.append(movie)
+    return output
+    # return cursor.fetchall()
+
+# @app.get('/movies/{movie_id}')
+# def get_single_movie(movie_id:int):  # put application's code here
+#     db = sqlite3.connect('movies.db')
+#     print(movie_id)
+#     cursor = db.cursor()
+#     movies = cursor.execute(f"SELECT * FROM movies WHERE id={movie_id}")
+#     output = []
+#     for movie in movies:
+#          movie = {'title': movie[1], 'year': movie[2], 'actors': movie[3]}
+#          output.append(movie)
+#     return output
+
+@app.get('/movies/{movie_id}')
+def get_single_movie(movie_id:int):  # put application's code here
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    movie = cursor.execute(f"SELECT * FROM movies WHERE id={movie_id}").fetchone()
+    if movie is None:
+        return {'message': "Movie not found"}
+    return {'title': movie[1], 'year': movie[2], 'actors': movie[3]}
+
+@app.post("/movies")
+def add_movie(movie: Movie):
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    cursor.execute(f"INSERT INTO movies (title, year, actors) VALUES ('{movie.title}', '{movie.year}', '{movie.actors}')")
+    db.commit()
+    return {"message": f"Movie with id = {cursor.lastrowid} added successfully"}
+    # movie = models.Movie.create(**movie.dict())
+    # return movie
+
+@app.post("/movies-new")
+def add_movie(params: dict[str, Any]):
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    # cursor.execute(f"INSERT INTO movies (title, year, actors) VALUES ('{params['title']}', '{params['year']}', '{params['actors']}')")
+    cursor.execute(f"INSERT INTO movies (title, year, actors) VALUES (?, ?, ?)", (params['title'], params['year'], params['actors']))
+    db.commit()
+
+    return {"message": f"Movie with id = {cursor.lastrowid} added successfully"}
+
+@app.put("/movies/{movie_id}")
+def update_movie(movie_id:int, params: dict[str, Any]):
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    cursor.execute(
+    "UPDATE movies SET title = ?, year = ?, actors = ? WHERE id = ?",
+    (params['title'], params['year'], params['actors'], movie_id)
+    )
+    db.commit()
+    if cursor.rowcount == 0:
+        return {"message": f"Movie with id = {movie_id} not found"}
+    return {"message": f"Movie with id = {cursor.lastrowid} updated successfully"}
+
+@app.delete("/movies/{movie_id}")
+def delete_movie(movie_id:int):
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM movies WHERE id = ?", (movie_id,))
+    db.commit()
+    if cursor.rowcount == 0:
+        return {"message": f"Movie with id = {movie_id} not found"}
+    return {"message": f"Movie with id = {movie_id} deleted successfully"}
+
+@app.delete("/movies")
+def delete_movies(movie_id:int):
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM movies")
+    db.commit()
+    return {"message": f"Deleted {cursor.rowcount} movies"}
 
 
-@app.post("/movies", response_model=schemas.Movie)
-def add_movie(movie: schemas.MovieBase):
-    movie = models.Movie.create(**movie.dict())
-    return movie
-
-
-@app.get("/movies/{movie_id}", response_model=schemas.Movie)
-def get_movie(movie_id: int):
-    db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    return db_movie
-
-
-@app.delete("/movies/{movie_id}", response_model=schemas.Movie)
-def get_movie(movie_id: int):
-    db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    db_movie.delete_instance()
-    return db_movie
+# if __name__ == '__main__':
+#     app.run()
